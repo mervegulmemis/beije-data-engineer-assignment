fda9d6d31509463563c9a6c04ae1ac1fe8ce1360
mervegulmemis@hotmail.com <mervegulmemis@hotmail.com>
2025-10-08 14:42:31 +0000

    adresses change

--- models/example/stg_adresses.sql
+++ models/example/stg_adresses.sql
@@ -2,11 +2,11 @@
 
 -- Adres verisinden son güncel kaydı almak için
 select
-  address_id,
-  customer_id,
+  invoiceType,
   trim(city) as city,
   trim(state) as state,
-  trim(country) as country,
+  trim(_neighborhood) as neighborhood,
+  trim(_country) as country,
   safe_cast(updated_at as timestamp) as updated_at
 from {{ source('Raw','addresses') }}
-where customer_id is not null
+where _user is not null
